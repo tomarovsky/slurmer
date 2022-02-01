7081a530889ac2626ec736d29ff208932a0ad09f
Tomarovsky <andrey.tomarovsky@gmail.com>
2022-02-01 12:09:41 +0300

    krater added

---
+++ krater/mmar_krater.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -l
+#SBATCH --job-name=krater.mmar
+#SBATCH --mail-type=END
+#SBATCH --mail-user=andrey.tomarovsky@gmail.com
+#SBATCH --cpus-per-task=32
+#SBATCH --mem=256gb
+#SBATCH --time=150:00:00
+#SBATCH --output=/mnt/tank/scratch/atomarovsky/krater.mmar.log
+#SBATCH --error=/mnt/tank/scratch/atomarovsky/krater.mmar.err       
+squeue; hostname; date;
+export TOOLS=/mnt/tank/scratch/atomarovsky/tools/
+
+conda activate jf
+
+cd /mnt/tank/scratch/skliver/common/mustelidae/martes_martes/genome/resequencing/raw/;
+
+for i in `ls */*`; do
+	mkdir ../../kmer/${i%%/*};
+	python3 $TOOLS/KrATER/scripts/jf/draw_kmer_distribution_from_fastq.py -i ${i%%.*}.1.fastq.gz,${i%%.*}.2.fastq.gz -o ../../kmer/${i%%/*}/${i%%/*} -m 23 -s 30G -t 10 -b;
+done
+
+echo 'finish!';
+date;
