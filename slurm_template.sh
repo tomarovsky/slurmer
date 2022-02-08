f1fc6260190515c902e7b8b9c9dd2216b7ab3b61
Tomarovsky <andrey.tomarovsky@gmail.com>
2022-02-08 03:18:49 +0300

    fix

--- slurm_template.sh
+++ slurm_template.sh
@@ -1,16 +1,16 @@
-#!/bin/bash
+#!/bin/bash -l
 #SBATCH --job-name=TEST
 #SBATCH --mail-type=END
 #SBATCH --mail-user=andrey.tomarovsky@gmail.com
 #SBATCH --cpus-per-task=32
 #SBATCH --mem=256gb
 #SBATCH --time=150:00:00
-#SBATCH --output=/nfs/home/atomarovsky/TEST.log
-#SBATCH --error=/nfs/home/atomarovsky/TEST.err       
+#SBATCH --output=/mnt/tank/scratch/atomarovsky/TEST.log
+#SBATCH --error=/mnt/tank/scratch/atomarovsky/TEST.err       
 squeue; hostname; date;
 export TOOLS=/mnt/tank/scratch/atomarovsky/tools/
 
-
+# script
 
 echo 'finish!';
 date;
---
+++ stirka_trimmomatic/mmar.stirka_trimmomatic.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -l
+#SBATCH --job-name=TEST
+#SBATCH --mail-type=END
+#SBATCH --mail-user=andrey.tomarovsky@gmail.com
+#SBATCH --cpus-per-task=32
+#SBATCH --mem=256gb
+#SBATCH --time=150:00:00
+#SBATCH --output=/mnt/tank/scratch/atomarovsky/TEST.log
+#SBATCH --error=/mnt/tank/scratch/atomarovsky/TEST.err       
+squeue; hostname; date;
+export TOOLS=/mnt/tank/scratch/atomarovsky/tools/
+
+cd /mnt/tank/scratch/atomarovsky; pwd;
+
+raw="/mnt/tank/scratch/atomarovsky/mmar_resequencing/raw/"
+filtered="/mnt/tank/scratch/atomarovsky/mmar_resequencing/filtered/"
+
+
+python3 $TOOLS/MAVR/scripts/filter/filter_stirka_trimmomatic.py -d ${raw} -x filtering.stats -o ${filtered} -t $SLURM_CPUS_PER_TASK -a $TOOLS/Trimmomatic-0.39/adapters/TruSeq2-PE.fa -k $TOOLS/service_seq/illumina_adapters_with_rev_com_23_mer.kmer -g 8 -j $TOOLS/Trimmomatic-0.39/ -r --stat_off -c $TOOLS/stirka/ -q 20 -l 50 -s S28,S29,S49,S50,T149,T151
+
+echo 'finish!';
+date;
